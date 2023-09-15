06ce82faaf184c4401540f98632fbbe725164944
sarkarniloy102 <sarkarniloy102@gmail.com>
2023-09-15 16:43:53 +0600

    header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './Component/Blogs/Blogs'
 import Header from './Component/Header/Header'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
